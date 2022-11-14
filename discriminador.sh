3310962df72f3eb2d5f7bd8733f5c42bb44659d4
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2022-11-14 18:17:22 +0000

    Update discriminador.sh

--- discriminador.sh
+++ discriminador.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-if["$EUID" - ne 0];
-then
+if [ "$EUID" -ne 0 ]; then
   echo ""
-  echo - e "\e[1;31m	B!EJECUTA ESTE SCRIPT COMO SUPERUSUARIO!	\e[0m"
+  echo -e "\e[1;31m !EJECUTA ESTE SCRIPT COMO SUPERUSUARIO! \e[0m"
   echo ""
 exit
 fi
   echo ""
-  read - p "	Introduzca el path: " FILE
-  size = $ (du - s $FILE | cut - f1)
-  goodsize = $ (echo "scale=2; $size / 1000;" | bc) if[-f $FILE];
-then
+  read -p " Introduzca el path: " FILE
+  size=$(du - s $FILE | cut - f1)
+  goodsize=$(echo "scale=2; $size / 1000;" | bc) 
+if [ -f $FILE ]; then
   echo ""
-  echo "	Esto es un ARCHIVO,"
-  echo "	el cual pesa $size bytes"
-  echo "	O $goodsize MB, que es mC!s comodo de leer"
-  echo "" elif[-d $FILE];
-then
+  echo "    Esto es un ARCHIVO,"
+  echo "    el cual pesa $size bytes"
+  echo "    O $goodsize MB, que es más comodo de leer"
+  echo "" 
+elif [ -d $FILE ]; then
   echo ""
-  echo "	Esto es un DIRECTORIO"
-  echo "	el cual pesa $size bytes,"
-  echo "	O $goodsize MB, que es mC!s comodo de leer" echo ""
+  echo "    Esto es un DIRECTORIO"
+  echo "    el cual pesa $size bytes,"
+  echo "    O $goodsize MB, que es más comodo de leer" 
+echo ""
 else
   echo ""
-  echo - e "\e[1;31m 	El path introducido no corresponde a nada :( \e[0m"
+  echo -e "\e[1;31m     El path introducido no corresponde a nada :( \e[0m"
   echo "" 
 fi
